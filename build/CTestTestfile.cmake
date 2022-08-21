5091c360e85c0b4e4dd979e1f75324340c23bd96
IAmAw-some <singalaadil@gmail.com>
2022-08-21 17:36:25 +0530

    changes

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/aadilsingal/Documents/GitHub/opengl-learning
+# Build directory: /Users/aadilsingal/Documents/GitHub/opengl-learning/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ src/main.cpp
@@ -0,0 +1,2 @@
+#include <cstdio>
+#include <glad/glad.h>
